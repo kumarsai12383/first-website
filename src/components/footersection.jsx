592ac401c3406627333fa0a4324298d4a1d93f02
kumarsai12383 <kumarsai05072005@gmail.com>
2026-04-14 10:06:43 +0530

    fixed footer

--- src/components/footersection.jsx
+++ src/components/footersection.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 function Footersection() {
     return (
-        <footer className="bg-white text-white flex items-center justify-center h-30 w-full ">
-            <div className="container mx-auto flex justify-between items-center text-center">
+        <footer className="bg-white text-white  h-20 w-full ">
+            <div className="max-w-6xl mx-auto mt-12 flex justify-between items-center text-center">
                 <h1 className="text-gray-700">HabitFlow</h1>
                 <p className="text-gray-500">&copy; 2024 HabitFlow. All rights reserved.</p>
                 <ul className="flex justify-center space-x-4 mt-4">
